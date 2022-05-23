1e36a2b75e3192caefef8db73a53cefe0e8ac73e
bradygaster <bradyg@microsoft.com>
2022-05-23 11:45:57 -0700

    trying this

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -4,12 +4,12 @@ using Yarp.ReverseProxy.Transforms;
 var builder = WebApplication.CreateBuilder(args);
 
 _ = builder.Services.AddReverseProxy()
-                    .AddTransforms(builderContext =>
-                    {
-                        builderContext.CopyRequestHeaders = true;
-                        builderContext.AddOriginalHost(useOriginal: true);
-                        builderContext.UseDefaultForwarders = true;
-                    })
+                    // .AddTransforms(builderContext =>
+                    // {
+                    //     builderContext.CopyRequestHeaders = true;
+                    //     builderContext.AddOriginalHost(useOriginal: true);
+                    //     builderContext.UseDefaultForwarders = true;
+                    // })
                     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
                         .AddConfigFilter<CustomConfigFilter>();
 
